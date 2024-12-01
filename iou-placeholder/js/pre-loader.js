8decfd32100319bd47dc19b76c4261fa0b595257
Nasir Uddin <74647944+nasiruddin-an4@users.noreply.github.com>
2024-12-01 17:51:16 +0600

    IOU Documentaion Landing page loader

--- iou-placeholder/js/pre-loader.js
+++ iou-placeholder/js/pre-loader.js
@@ -4,10 +4,10 @@
   /*============= preloader js css =============*/
   var cites = [];
   cites[0] =
-    "We design Docy for the readers, optimizing not for page views or engagement";
-  cites[1] = "Docy turns out that context is a key part of learning.";
-  cites[2] = "You can create any type of product documentation with Docy";
-  cites[3] = "Advanced visual search system powered by Ajax";
+    "We design IOU for the readers, optimizing not for page views or engagement";
+  cites[1] = "IOU turns out that context is a key part of learning.";
+  cites[2] = "You can create any type of product documentation with IOU";
+  cites[3] = "Advanced visual search system powered by IOU";
   var cite = cites[Math.floor(Math.random() * cites.length)];
   $("#preloader p").text(cite);
   $("#preloader").addClass("loading");
